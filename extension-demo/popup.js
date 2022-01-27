a6e8656f36b35956958e38437beb14cdfe23b3a4
itsket <57805702+itsket@users.noreply.github.com>
2022-01-27 13:17:44 -0500

    Add files via upload

---
+++ extension-demo/popup.js
@@ -0,0 +1,9 @@
+function CLICK() {
+  document.getElementById("problem").innerHTML="<p> just testing </p>";
+  document.getElementById("js").setAttribute("src","img/icon48.png");v
+document.getElementById("js").style.display = "none";
+
+}
+
+<div id="problem" class="problem">
+ </div>
